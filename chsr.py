04d06334c1290b30ae5d750f77315d123c272739
Mauro Faccin <mauro.fccn@gmail.com>
2026-05-08 12:00:49 +0200

    add base funcs

---
+++ chsr.py
@@ -0,0 +1,7 @@
+"""Base function and data."""
+
+from pathlib import Path
+
+DATAPATH = Path("chinese_high-speed_railways/high-speed trains operation data.csv.gz")
+CN_RAILS = Path("data/CN_railways.gpkg")
+CN_STATION_LOC = Path("data/stations.gpkg")
